9bccc57db0b0b2e3d42ef323d7b7f77627a0b034
Simon Arida <me@Simons-MacBook-Pro.local>
2017-05-15 15:53:13 +0300

    first commit

---
+++ Hello-World/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hello-World
+//
+//  Created by Simon Arida on 5/15/17.
+//  Copyright © 2017 NM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
